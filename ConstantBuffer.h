a87e22b62ab70354760ed3bff604b1d8ac3e469e
wewe231223@Laptop <wewe231223@kpu.ac.kr>
2024-01-14 16:44:11 +0900

    Add Mapped Switch

--- ConstantBuffer.h
+++ ConstantBuffer.h
@@ -4,10 +4,11 @@ class ConstantBuffer{
 public:
 	ConstantBuffer();
 	ConstantBuffer(ComPtr<ID3D12Device> d3dDevice);
-
+	~ConstantBuffer();
 private:
 	ComPtr<ID3D12Resource> m_d3dConstantBuffer{ nullptr };
 	T* m_constantInterface{ nullptr };
+	bool m_bMapped{ false };
 public:
 	void CopyData(const T& Data);
 	D3D12_GPU_VIRTUAL_ADDRESS GetVirtualAddress() const;
@@ -34,11 +35,18 @@ ConstantBuffer<T>::ConstantBuffer(ComPtr<ID3D12Device> d3dDevice){
 }
 
 template<typename T>
+inline ConstantBuffer<T>::~ConstantBuffer(){
+	if (m_bMapped) m_d3dConstantBuffer->Unmap(0, nullptr);
+}
+
+template<typename T>
 inline void ConstantBuffer<T>::CopyData(const T& Data) {
 	T* Temp{nullptr};
 	ThrowIfFailed(m_d3dConstantBuffer->Map(0, nullptr, reinterpret_cast<void**>(&Temp)));
+	m_bMapped = true;
 	::memcpy(Temp, &Data, sizeof(T));
 	m_d3dConstantBuffer->Unmap(0, nullptr);
+	m_bMapped = false;
 }
 
 template<typename T>
